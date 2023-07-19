d150734b4476de57bad1e9cc453397d054b6c791
Victoria Binimelis Vadell <vbinimelis@hotelbeds.com>
2023-07-19 16:41:48 +0200

    redisseny HomePage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -10,10 +10,11 @@ class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
-// 801c24
-Color _grana = Color.fromARGB(0xFF, 0x6C, 0x1C, 0x24);
 
 class _HomePageState extends State<HomePage> {
+  // Color personalizado: #801c24
+  Color _grana = Color.fromARGB(0xFF, 0x6C, 0x1C, 0x24);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,81 +26,108 @@ class _HomePageState extends State<HomePage> {
         },
       ),
       body: Container(
-        child: Stack(
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                  child: Image.asset(
-                    "assets/img/logo_grana.png",
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: 200,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade300,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                "assets/img/logo_grana.png",
+                width: MediaQuery.of(context).size.width / 2,
+                height: 200,
+              ),
+              SizedBox(height: 50),
+              Container(
+                width: MediaQuery.of(context).size.width *
+                    0.6, // Ajusta el valor para modificar el tamaño del botón
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ProcesPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white, // Fondo blanco para el botón
+                    foregroundColor: _grana, // Color del texto
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          30), // Ajusta la curvatura de las esquinas
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 20), // Ajusta el tamaño del botón
                   ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.4, // Ajusta el valor para modificar el tamaño del botón
-                  child: ElevatedButton(
-                    child: const Text('En procès'),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ProcesPage()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: _grana, // background color
-                      onPrimary: Colors.white, // foreground color
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20), // Ajusta el valor para modificar la curvatura de las esquinas
-                      ),
-                      padding: const EdgeInsets.all(16), // Ajusta el valor para modificar el tamaño interno del botón
+                  child: Text(
+                    'En procès',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.4, // Ajusta el valor para modificar el tamaño del botón
-                  child: ElevatedButton(
-                    child: const Text('Galeria'),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => GaleriaPage()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: _grana, // background color
-                      onPrimary: Colors.white, // foreground color
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20), // Ajusta el valor para modificar la curvatura de las esquinas
-                      ),
-                      padding: const EdgeInsets.all(16), // Ajusta el valor para modificar el tamaño interno del botón
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => GaleriaPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white, // Fondo blanco para el botón
+                    foregroundColor: _grana, // Color del texto
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          30), // Ajusta la curvatura de las esquinas
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 20), // Ajusta el tamaño del botón
+                  ),
+                  child: Text(
+                    'Galeria',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.4, // Ajusta el valor para modificar el tamaño del botón
-                  child: ElevatedButton(
-                    child: const Text('Sortir'),
-                    onPressed: () {
-                      SystemChannels.platform.invokeMethod('SystemNavigator.pop');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: _grana, // background color
-                      onPrimary: Colors.white, // foreground color
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20), // Ajusta el valor para modificar la curvatura de las esquinas
-                      ),
-                      padding: const EdgeInsets.all(16), // Ajusta el valor para modificar el tamaño interno del botón
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: ElevatedButton(
+                  onPressed: () {
+                    SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white, // Fondo blanco para el botón
+                    foregroundColor: _grana, // Color del texto
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          30), // Ajusta la curvatura de las esquinas
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 20), // Ajusta el tamaño del botón
+                  ),
+                  child: Text(
+                    'Sortir',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-              ],
-            )
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
